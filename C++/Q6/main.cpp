a453d2cb094b4bfd6057c3c6ae08055d6bbbb43f
Ahmadreza <ahmadrezaanazari@gmail.com>
2019-05-03 15:54:52 +0430

    Q6 finished

--- C++/Q6/main.cpp
+++ C++/Q6/main.cpp
@@ -1,40 +1,71 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
+
+template <typename T>
+void printContainer(const T&, std::string);
+
+template <typename T>
+void printContainer(const T&, const T& end);
 
 int main()
 {
     std::vector<int> vec{1, 2, 3, 4, 5, 4, 3, 2, 1};
 
-    std::cout << "original vector:\n";
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        std::cout << vec[i] << ", ";
-    }
-    std::cout << std::endl;
+     printContainer(vec, "original vector:");
 
     auto firstToDelete{std::remove(vec.begin(), vec.end(), 2)};
 
-    std::cout << "after remove:\n";
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << ", ";
-    std::cout << std::endl;
+    printContainer(vec, "after remove:");
 
     vec.erase(firstToDelete, vec.end());
 
-    std::cout << "after erase:\n";
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << ", ";
-    std::cout << std::endl;
+    printContainer(vec, "after erase:");
 
     std::for_each(vec.begin(), vec.end(), [](int &i) { i *= 2; });
 
-    std::cout << "doubling members:\n";
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << ", ";
-    std::cout << std::endl;
+    printContainer(vec, "members doubled:");
 
     double avg{};
-    std::sort(vec.begin(), vec.end(), [&avg](const auto& a, const auto& b){ return std::abs(a - avg) < std::abs(b - avg)});
+    std::for_each(vec.begin(), vec.end(), [&avg](const int &i) { avg += i; });
+    avg /= vec.size();
+    std::sort(vec.begin(), vec.end(), [&avg](const auto &a, const auto &b) { return std::abs(a - avg) < std::abs(b - avg); });
+
+    printContainer(vec, "members sorted by nearity to the average:");
+
+    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+
+    printContainer(vec, "duplicated members erased");
+
+    std::set<int> s{};
+    for (size_t i = 0; i < vec.size(); i++) {
+      s.insert(vec[i]);
+    }
+
+    printContainer(s, "set created");
+
+
+
+    s.erase(std::find_if(s.begin(), s.end(), [](const int &i){return i > 3;}), s.end());
+
+    printContainer(s, "elemnts greater than3 erased");
     return 0;
+}
+
+template <typename T>
+void printContainer(const T& list, std::string message)
+{
+    std::cout << message << std::endl;
+    printContainer(list.begin(), list.end());
+    std::cout << std::endl;
+}
+
+template <typename T>
+void printContainer(const T& begin, const T& end) {
+  T it{begin};
+  while (it != end) {
+    std::cout << *it << ", ";
+    it++;
+  }
 }
