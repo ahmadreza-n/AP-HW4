ed28d3319f8c0cedbe039e86a20543e333904e3b
Ahmadreza <ahmadrezaanazari@gmail.com>
2019-05-04 11:14:00 +0430

    indeted

--- C++/Q6/main.cpp
+++ C++/Q6/main.cpp
@@ -4,57 +4,55 @@
 #include <set>
 
 template <typename T>
-void printContainer(const T&, std::string);
-
+void printContainer(const T &, std::string);
 
 int main()
 {
-    std::vector<int> vec{1, 2, 3, 4, 5, 4, 3, 2, 1};
+  std::vector<int> vec{1, 2, 3, 4, 5, 4, 3, 2, 1};
 
-     printContainer(vec, "original vector:");
+  printContainer(vec, "original vector:");
 
-    auto firstToDelete{std::remove(vec.begin(), vec.end(), 2)};
+  auto firstToDelete{std::remove(vec.begin(), vec.end(), 2)};
 
-    printContainer(vec, "after remove:");
+  printContainer(vec, "after remove:");
 
-    vec.erase(firstToDelete, vec.end());
+  vec.erase(firstToDelete, vec.end());
 
-    printContainer(vec, "after erase:");
+  printContainer(vec, "after erase:");
 
-    std::for_each(vec.begin(), vec.end(), [](int &i) { i *= 2; });
+  std::for_each(vec.begin(), vec.end(), [](int &i) { i *= 2; });
 
-    printContainer(vec, "members doubled:");
+  printContainer(vec, "members doubled:");
 
-    double avg{};
-    std::for_each(vec.begin(), vec.end(), [&avg](const int &i) { avg += i; });
-    avg /= vec.size();
-    std::sort(vec.begin(), vec.end(), [&avg](const auto &a, const auto &b) { return std::abs(a - avg) < std::abs(b - avg); });
+  double avg{};
+  std::for_each(vec.begin(), vec.end(), [&avg](const int &i) { avg += i; });
+  avg /= vec.size();
+  std::sort(vec.begin(), vec.end(), [&avg](const auto &a, const auto &b) { return std::abs(a - avg) < std::abs(b - avg); });
 
-    printContainer(vec, "members sorted by nearity to the average:");
+  printContainer(vec, "members sorted by nearity to the average:");
 
-    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+  vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
 
-    printContainer(vec, "duplicated members erased");
+  printContainer(vec, "duplicated members erased");
 
-    std::set<int> s{};
-    for (size_t i = 0; i < vec.size(); i++) {
-      s.insert(vec[i]);
-    }
+  std::set<int> s{};
+  for (size_t i = 0; i < vec.size(); i++)
+  {
+    s.insert(vec[i]);
+  }
 
-    printContainer(s, "set created");
+  printContainer(s, "set created");
 
+  s.erase(std::find_if(s.begin(), s.end(), [](const int &i) { return i > 3; }), s.end());
 
-
-    s.erase(std::find_if(s.begin(), s.end(), [](const int &i){return i > 3;}), s.end());
-
-    printContainer(s, "elemnts greater than3 erased");
-    return 0;
+  printContainer(s, "elemnts greater than3 erased");
+  return 0;
 }
 
 template <typename T>
-void printContainer(const T& list, std::string message)
+void printContainer(const T &list, std::string message)
 {
-    std::cout << message << std::endl;
-    std::for_each(list.begin(), list.end(), [](const int &i ){std::cout << i << ", ";});
-    std::cout << std::endl;
+  std::cout << message << std::endl;
+  std::for_each(list.begin(), list.end(), [](const int &i) { std::cout << i << ", "; });
+  std::cout << std::endl;
 }
--- C++/Q7/main.cpp
+++ C++/Q7/main.cpp
@@ -5,37 +5,38 @@
 #include <cmath>
 
 template <typename T>
-void printContainer(const T&, std::string);
+void printContainer(const T &, std::string);
 
-int main() {
-    std::vector<int> a(50);
-    std::vector<int> b(50);
-    srand(time(NULL));
-    std::for_each(b.begin(), b.end(), [](int& i){i = rand() % 50 + 1;});
+int main()
+{
+  std::vector<int> a(50);
+  std::vector<int> b(50);
+  srand(time(NULL));
+  std::for_each(b.begin(), b.end(), [](int &i) { i = rand() % 50 + 1; });
 
-    printContainer(a, "a is:");
-    printContainer(b, "b is:");
-
-    b.erase(std::unique(b.begin(), b.end()), b.end());
+  printContainer(a, "a is:");
   printContainer(b, "b is:");
-    std::vector<int> c(50);
-    int num{1};
-    std::for_each(c.begin(), c.end(), [&num](int &i){i = num; num++;});
 
-    printContainer(c, "c is:");
+  b.erase(std::unique(b.begin(), b.end()), b.end());
+  printContainer(b, "b is:");
+  std::vector<int> c(50);
+  int num{1};
+  std::for_each(c.begin(), c.end(), [&num](int &i) {i = num; num++; });
 
-    b.resize(50);
-    printContainer(b, "b is:");
-    std::vector<int>::iterator iterb{b.begin()}, iterc{c.begin()};
-    std::for_each(a.begin(), a.end(), [&iterb, &iterc](int &i){i = std::pow(*(iterc++), 2) - std::pow(*(iterb++), 2); });
-    printContainer(a, "a is:");
+  printContainer(c, "c is:");
+
+  b.resize(50);
+  printContainer(b, "b is:");
+  std::vector<int>::iterator iterb{b.begin()}, iterc{c.begin()};
+  std::for_each(a.begin(), a.end(), [&iterb, &iterc](int &i) { i = std::pow(*(iterc++), 2) - std::pow(*(iterb++), 2); });
+  printContainer(a, "a is:");
   return 0;
 }
 
 template <typename T>
-void printContainer(const T& list, std::string message)
+void printContainer(const T &list, std::string message)
 {
-    std::cout << message << std::endl;
-    std::for_each(list.begin(), list.end(), [](const int &i ){std::cout << i << ", ";});
-    std::cout << std::endl;
+  std::cout << message << std::endl;
+  std::for_each(list.begin(), list.end(), [](const int &i) { std::cout << i << ", "; });
+  std::cout << std::endl;
 }
